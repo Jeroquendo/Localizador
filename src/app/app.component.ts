db2bec2533b1c28075b3eb0ad7102b46b7356131
Jeroquendo <Jeroquendo@gmail.com>
2019-10-02 18:35:41 -0500

    Actualización de posisción

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Minimatch } from 'minimatch';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Localizador-front';
-  lat = 51.678418;
-  lng = 7.809007;
+  max = 0;
+  min = 0;
+  maxC = 0;
+  minC = 0;
+  control = 0;
+  lat = 6.2486;
+  lng = -75.5742;
+  constructor(){
+    this.myFunction();
+  }
+
+  myFunction() {
+    setInterval(()=>{
+    console.log("cambio");
+    this.max = 3;
+    this.min = 0;
+    this.control = this.lat + Math.random() * (this.maxC - this.minC) + this.minC;
+    this.max = 0.1;
+    this.min = 0.01;
+    if(this.control==0){
+      this.lat = this.lat + Math.random() * (this.max - this.min) + this.min;
+      this.lng = this.lng + Math.random() * (this.max - this.min) + this.min;
+    } else if(this.control==1){
+      this.lat = this.lat + Math.random() * (this.max - this.min) + this.min;
+      this.lng = this.lng - Math.random() * (this.max - this.min) + this.min;
+    } else if(this.control==2){
+      this.lat = this.lat - Math.random() * (this.max - this.min) + this.min;
+      this.lng = this.lng + Math.random() * (this.max - this.min) + this.min;
+    } else{
+      this.lat = this.lat - Math.random() * (this.max - this.min) + this.min;
+      this.lng = this.lng - Math.random() * (this.max - this.min) + this.min;
+    } 
+    }, 2000);
+  }
+
 }
+
